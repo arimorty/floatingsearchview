b630078bdc47b2488a8715eaafd5c03b625327b6
MortyC <mortychesh@gmail.com>
2015-12-08 10:46:12 -0500

    clean sample

--- sample/src/main/java/com/arlib/floatingsearchviewdemo/MainActivity.java
+++ sample/src/main/java/com/arlib/floatingsearchviewdemo/MainActivity.java
@@ -226,8 +226,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDrawerStateChanged(int newState) { }
         });
-
-        mSearchView.setSearchFocused(true);
     }
 
     private void refreshBackgroundColor(String colorName, String colorValue){
