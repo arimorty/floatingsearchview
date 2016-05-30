52cddad4cd5960ac9cd6b93b0e74b2a8041c85dc
Ari (personal) <mortychesh@gmail.com>
2016-05-29 22:17:13 -0400

    update copyright notice

--- library/src/main/java/com/arlib/floatingsearchview/suggestions/model/SearchSuggestion.java
+++ library/src/main/java/com/arlib/floatingsearchview/suggestions/model/SearchSuggestion.java
@@ -19,6 +19,22 @@ package com.arlib.floatingsearchview.suggestions.model;
 import android.os.Parcelable;
 
 /**
+ * Copyright (C) 2016 Arlib
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * An object that represents a single suggestion item
  * in the suggestions drop down generated in response
  * to an entered query in the {@link com.arlib.floatingsearchview.FloatingSearchView}
--- sample/src/main/java/com/arlib/floatingsearchviewdemo/data/ColorSuggestion.java
+++ sample/src/main/java/com/arlib/floatingsearchviewdemo/data/ColorSuggestion.java
@@ -5,12 +5,19 @@ import android.os.Parcel;
 import com.arlib.floatingsearchview.suggestions.model.SearchSuggestion;
 
 /**
- * Copyright (c) 2015 Ari C
+ * Copyright (C) 2016 Arlib
  * <p/>
- * ColorSuggestion ---
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * <p/>
- * Author Ari
- * Created on 10/19/2015.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 public class ColorSuggestion implements SearchSuggestion {
 
