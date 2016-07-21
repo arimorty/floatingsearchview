3bac6432b0eac16808058f528d11be7f9a451479
Ari <cheshinmortya@gmail.com>
2016-07-20 20:32:13 -0400

    add a missing case to fix in PR #85

--- library/src/main/java/com/arlib/floatingsearchview/suggestions/SearchSuggestionsAdapter.java
+++ library/src/main/java/com/arlib/floatingsearchview/suggestions/SearchSuggestionsAdapter.java
@@ -92,8 +92,10 @@ public class SearchSuggestionsAdapter extends RecyclerView.Adapter<RecyclerView.
                 @Override
                 public void onClick(View v) {
 
-                    if (mListener != null)
+                    int adapterPosition = getAdapterPosition();
+                    if (mListener != null && adapterPosition != RecyclerView.NO_POSITION) {
                         mListener.onMoveItemToSearchClicked(getAdapterPosition());
+                    }
                 }
             });
 
